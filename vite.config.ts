7908a426c6553b2383f8261570aa1fcb7d6dd505
rhochmayr <robert.hochmayr@gmx.at>
2025-06-23 19:34:03 +0200

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,14 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/cloud-management-portal/',
+  base: '',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
-  }
+  },
+  build: {
+    rollupOptions: {
+      input: 'index.html', // Tells Vite to use this HTML for bundling
+    },
+  }  
 });
